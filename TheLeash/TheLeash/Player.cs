716192d5f0d37852de79170c466842b4413b4e2c
JackKell <Jackkell@Citadel>
2014-11-08 21:22:29 -0700

    Fix Commit

--- TheLeash/TheLeash/Player.cs
+++ TheLeash/TheLeash/Player.cs
@@ -83,7 +83,7 @@ namespace TheLeash
             this.currentAnimationName = "";
         }
 
-        public void LoadContent(ContentManager content)
+        public virtual void LoadContent(ContentManager content)
         {
 
         }
